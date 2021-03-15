535c643bd4a8aedd17fa3d21cb9323bd2777eeeb
ErikNabiev <er2000nabiev@mail.ru>
2021-03-15 22:42:21 +0300

    fix

--- testServer.go
+++ testServer.go
@@ -10,7 +10,7 @@ func main() {
 		Addr: ":8887",
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			name := r.URL.Query().Get("name")
-			familia := r.URL.Query().Get("familia")
+			//familia := r.URL.Query().Get("familia")
 			result := "Ваше имя: " + name + " Ваша фамилия: " + "nabiev"
 			w.WriteHeader(200)
 			w.Write([]byte(result))
